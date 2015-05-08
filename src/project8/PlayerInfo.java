fb18b20f9857415eb7ae0d6276e8287fce2cca24
yousef.m101@gmail.com <>
2015-05-08 19:20:05 +0500

    project 8 complete

---
+++ src/project8/PlayerInfo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project8;
+
+/**
+ *
+ * @author Ali
+ */
+public class PlayerInfo {
+    public String firstName;
+    public String lastName;
+    public int swipeCardNum ;
+    
+    public PlayerInfo(){
+            firstName=null;
+            lastName =null;
+        }
+    
+    
+    
+        public PlayerInfo(String fn , String ln){
+        
+            firstName = fn;
+            lastName = ln;
+        
+        }
+        
+        
+        
+        public String getFirstname(){
+            return firstName;
+        }
+        
+        public String getLastName(){
+            return lastName;
+        }
+        
+        public void setSwipeCard( int num){
+                swipeCardNum=num;
+        }
+        
+        
+        public int getSwipeCard( ){
+                return swipeCardNum;
+        }
+                
+                
+        
+        
+        
+}
---
+++ src/project8/Project8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project8;
+
+/**
+ *
+ * @author Ali
+ */
+public class Project8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        getSwipeCard swipeCard = new getSwipeCard();
+        swipeCard.setVisible(true);
+    }
+}
